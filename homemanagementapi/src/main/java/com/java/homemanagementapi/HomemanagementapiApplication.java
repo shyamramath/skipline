a33ba6f703f46aba6551d5437042befc3529e2ad
shyamramath <shyam.ramath@gmail.com>
2026-01-09 19:17:04 -0600

    Initial push

---
+++ homemanagementapi/src/main/java/com/java/homemanagementapi/HomemanagementapiApplication.java
@@ -0,0 +1,13 @@
+package com.java.homemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomemanagementapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomemanagementapiApplication.class, args);
+    }
+
+}
